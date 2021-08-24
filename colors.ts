6477bd1c44331360602abe8528da8557251bc5ac
mattstraussHotelEngine <80705586+mattstraussHotelEngine@users.noreply.github.com>
2021-08-24 15:40:13 -0600

    Updated the colors to reflect the scale before actually defining them in the theme

---
+++ colors.ts
@@ -0,0 +1,308 @@
+/**
+ * Instructions: If it fits within scale, add it to the color scale, if not add the value as a "alias" like below
+ * Colors were named with the help of this color naming utility: https://colornamer.netlify.app/
+ * The alias colors are from the old/current state of things and should be removed or converted when deemed appropriate/deprecated
+ * */
+
+export const colors = {
+  /** Value: #FFFFFF */
+  white: "#FFFFFF", // Core Brand - White
+  /** Value: #000000 */
+  black: "#000000", // Core Brand - Black
+  /** Value: #2F58EB */
+  retroBlue: "#2F58EB", // Core Brand - Blue
+  /** Value: #6d8af1 */
+  softBlue: "#6d8af1", // Core Brand - Blue Faded
+  /** Value: #1A1A1A */
+  errieBlack: "#1A1A1A", // Primary Content
+  /** Value: #414243 */
+  blackPanther: "#414243", // Secondary Content - Grey
+  /** Value: #929395 */
+  kiwiGrey: "#929395", // Tertiary Content - Grey
+  /** Value: #BDBEC1 */
+  piedWagtailGrey: "#BDBEC1", // Icon/Action - Grey
+  /** Value: #DDDEE0 */
+  azureHint: "#DDDEE0", // Borders - Grey
+  /** Value: #E7E8EC */
+  purpleCrystal: "#E7E8EC", // Dividers - Grey
+  /** Value: #F3F4F7 */
+  chefsHat: "#F3F4F7", // Background - Grey
+  /** Value: #008805 */
+  finePine: "#008805", // Success - Green
+  /** Value: #F2F9F2 */
+  peacefulRain: "#F2F9F2", // Success - Background - Light Green
+  /** Value: #FF3535 */
+  tomato: "#FF3535", // Error - Red
+  /** Value: #FEEEEE */
+  voraciousWhite: "#FEEEEE", // Error - Background - Light Red
+  /** Value: #FF9118 */
+  carrotCurl: "#FF9118", // Caution - Orange
+  /** Value: #FFF4E8 */
+  whatsLeft: "#FFF4E8", // Caution - Background - Light Orange
+  /** Value: #EDF3FC */
+  placeboBlue: "#EDF3FC", // Highlight - Background - Blue
+  /** Value: #95A0AA */
+  frilledShark: "#95A0AA", // Rewards - Silver - Grey
+  /** Value: #F5C25C */
+  butterfield: "#F5C25C", // Rewards - Gold - Yellow
+  /** Value: #6E7E8E */
+  chainGangGrey: "#6E7E8E", // Rewards - Platinum - Grey
+  /** Value: #F8892F */
+  brightMango: "#F8892F", // Rewards - VIP - Orange
+  /** Value: #6243BC */
+  dragonlordPurple: "#6243BC", // Loyalty - Primary - Purple
+  /** Value: #EFECF8 */
+  brightDusk: "#EFECF8", // Loyalty - Background - Light Purple
+  /** Value: #2646BC */
+  denim: "#2646BC", // Interactive - Primary - Blue
+  /** Value: #1C348D */
+  bavarianBlue: "#1C348D", // Interactive - Primary Active/Clicked - Blue
+  /** Value: rgba(47, 88, 235, 0.7) */
+  fadedRetroBlue: "rgba(47, 88, 235, 0.7)", // Interactive - Primary Disabled - Blue
+  /** Value: #FAFAFA */
+  drWhite: "#FAFAFA", // Interactive - Secondary Hover - White
+  /** Value: #F5F5F5 */
+  whiteSmoke: "#F5F5F5", // Interactive - Secondary Active/Clicked - White
+  /** Value: #D54A4A */
+  fadedRed: "#D54A4A", // Interactive - Delete Hover - Red
+  /** Value: #BE4242 */
+  rubiate: "#BE4242", // Interactive - Delete Active/Clicked - Red
+  /** Value: #ED5252 */
+  khmerCurry: "#ED5252", // Interactive - Delete Disabled -
+  /** Value: #D9753B */
+  orangeOchre: "#D9753B", // Avatar - Orange1
+  /** Value: #D9873B */
+  dullOrange: "#D9873B", // Avatar - Orange2
+  /** Value: #F2B279 */
+  excitingOrange: "#F2B279", // Avatar - Orange3
+  /** Value: #D97E8E */
+  loveYouPink: "#D97E8E", // Avatar - Pink
+  /** Value: #5856B2 */
+  moonlightPurple: "#5856B2", // Avatar - Purple
+  /** Value: #333A49 */
+  blueNights: "#333A49", // Avatar - Blue1
+  /** Value: #305473 */
+  aerostaticsBlue: "#305473", // Avatar - Blue2
+  /** Value: #3E64AD */
+  irisBlue: "#3E64AD", // Avatar - Blue3
+  /** Value: #739BD8 */
+  iceBlue: "#739BD8", // Avatar - Blue4
+  /** Value: #8FA4BF */
+  frillsBlue: "#8FA4BF", // Avatar - Blue5
+  /** Value: #3D88A9 */
+  magicBlue: "#3D88A9", // Avatar - Teal1
+  /** Value: #44ADC5 */
+  dragonflyBlue: "#44ADC5", // Avatar - Teal2
+  /** Value: #30A690 */
+  stampPadGreen: "#30A690", // Avatar - Teal3
+  /** Value: #6AA95A */
+  technoGreen: "#6AA95A", // Avatar - Green1
+  /** Value: #92CB59 */
+  cabbagePatch: "#92CB59", // Avatar - Green2
+  /** Value: #E7C54D */
+  indianMaize: "#E7C54D", // Avatar - Yellow
+  /** Value: #A65A49 */
+  bruschettaRed: "#A65A49", // Avatar - Red1
+  /** Value: #CF5151 */
+  chestnutRose: "#CF5151", // Avatar - Red2
+  /** Value: #747A8C */
+  fantasiaGrey: "#747A8C", // Avatar - Grey1
+  /** Value: #8C8987 */
+  assentGrey: "#8C8987", // Avatar - Grey2
+  /** Value: rgba(0, 0, 0, 0.12) */
+  lightShadow: "rgba(0, 0, 0, 0.12)", // Button Shadow - Top
+  /** Value: rgba(0, 0, 0, 0.04) */
+  darkShadow: "rgba(0, 0, 0, 0.04)", // Button Shadow - Bottom
+  /** Value: #268346 */
+  richGreen: `#268346`, // Dashboard Trends Green
+  /** Value: #EAEBEE */
+  lavendarWisp: `#EAEBEE`, // Dashboard Billing Progress Background
+  /** Value: #430CF3 */
+  royalHunterBlue: `#430CF5`, // Dashboard Billing Icon
+  /** Value: #f1e6ff */
+  placeboPurple: `#f1e6ff`, // Dashboard Billing Icon Background
+  /** Value #a3a4a6 */
+  powerGrey: "#a3a4a6", // Dashboard Billing Grey
+  red: {
+    /** Value: #FBEBED*/
+    50: "#FBEBED",
+    /** Value: #F0B0B9*/
+    100: "#F0B0B9",
+    /** Value: */
+    200: "#E88996",
+    /** Value: #E16173*/
+    300: "#E16173",
+    /** Value: #D93A50*/
+    400: "#D93A50",
+    /** Value: #AE2E40*/
+    500: "#AE2E40",
+    /** Value: #822330*/
+    600: "#822330",
+    /** Value: #571720*/
+    700: "#571720",
+  },
+  orange: {
+    /** Value: #FEF3EA */
+    50: "#FEF3EA",
+    /** Value: #FCD0AC*/
+    100: "#FCD0AC",
+    /** Value: #FBB882 */
+    200: "#FBB882",
+    /** Value: #F9A159 */
+    300: "#F9A159",
+    /** Value: #F8892F*/
+    400: "#F8892F",
+    /** Value: #C66E26*/
+    500: "#C66E26",
+    /** Value: #95521C*/
+    600: "#95521C",
+    /** Value: #633713*/
+    700: "#633713",
+  },
+  gold: {
+    /** Value: #FEF9EF*/
+    50: "#FEF9EF",
+    /** Value: #FBE7BE*/
+    100: "#FBE7BE",
+    /** Value: #F9DA9D*/
+    200: "#F9DA9D",
+    /** Value: #F7CE7D*/
+    300: "#F7CE7D",
+    /** Value: #F5C25C*/
+    400: "#F5C25C",
+    /** Value: #C2B052*/
+    500: "#C2B052",
+    /** Value: #937437*/
+    600: "#937437",
+    /** Value: #624E25*/
+    700: "#624E25",
+  },
+  yellow: {
+    /** Value: #FEFBF0*/
+    50: "#FEFBF0",
+    /** Value: #FEFBF0*/
+    100: "#FEFBF0",
+    /** Value: #FAF1C2*/
+    200: "#FAF1C2",
+    /** Value: */
+    300: "#F7EAA4",
+    /** Value: #F2DC67*/
+    400: "#F2DC67",
+    /** Value: #C2B052*/
+    500: "#C2B052",
+    /** Value: #91843E*/
+    600: "#91843E",
+    /** Value: #615829*/
+    700: "#615829",
+  },
+  green: {
+    /** Value: #ECFBF1*/
+    50: "#ECFBF1",
+    /** Value: #B3F1C8*/
+    100: "#B3F1C8",
+    /** Value: #8CE9AC */
+    200: "#8CE9AC",
+    /** Value: #66E291*/
+    300: "#66E291",
+    /** Value: #40DB75*/
+    400: "#40DB75",
+    /** Value: #33AF5E*/
+    500: "#33AF5E",
+    /** Value: #268346*/
+    600: "#268346",
+    /** Value:#1A582F */
+    700: "#1A582F",
+  },
+  aqua: {
+    /** Value: #EAFCFC*/
+    50: "#EAFCFC",
+    /** Value: #AAF1F1*/
+    100: "#AAF1F1",
+    /** Value: #80EBEB*/
+    200: "#80EBEB",
+    /** Value: #55E4E4*/
+    300: "#55E4E4",
+    /** Value: #2BDDDD*/
+    400: "#2BDDDD",
+    /** Value: #22B1B1*/
+    500: "#22B1B1",
+    /** Value: #1A8585 */
+    600: "#1A8585",
+    /** Value: #115858*/
+    700: "#115858",
+  },
+  blue: {
+    /** Value: #E9F5FE*/
+    50: "#E9F5FE",
+    /** Value: #A7D8FC*/
+    100: "#A7D8FC",
+    /** Value: #7BC5FB*/
+    200: "#7BC5FB",
+    /** Value: #4FB1F9*/
+    300: "#4FB1F9",
+    /** Value: #239EF8*/
+    400: "#239EF8",
+    /** Value: #1C7EC6*/
+    500: "#1C7EC6",
+    /** Value: #155F95*/
+    600: "#155F95",
+    /** Value: #0E3F63*/
+    700: "#0E3F63",
+  },
+  purple: {
+    /** Value: #EDE5FF*/
+    50: "#EDE5FF",
+    /** Value: #B799FF*/
+    100: "#B799FF",
+    /** Value: #9366FF*/
+    200: "#9366FF",
+    /** Value: #6F33FF*/
+    300: "#6F33FF",
+    /** Value: #4B00FF*/
+    400: "#4B00FF",
+    /** Value: #3C00CC*/
+    500: "#3C00CC",
+    /** Value: #2D0099*/
+    600: "#2D0099",
+    /** Value: #1E0066*/
+    700: "#1E0066",
+  },
+  slate: {
+    /** Value: */
+    50: "#ECF1F6",
+    /** Value: */
+    100: "#D1DBE7",
+    /** Value: */
+    200: "#A7BACC",
+    /** Value: */
+    300: "#8FA1B4",
+    /** Value: */
+    400: "#6E7E8E",
+    /** Value: */
+    500: "#586572",
+    /** Value: */
+    600: "#424C55",
+    /** Value: */
+    700: "#2C3239",
+  },
+  grey: {
+    /** Value: #F3F4F7*/
+    50: "#F3F4F7",
+    /** Value: #E7E8EC*/
+    100: "#E7E8EC",
+    /** Value: #DDDEE0*/
+    200: "#DDDEE0",
+    /** Value: #CBCBCB*/
+    300: "#CBCBCB",
+    /** Value: #AFAFAF*/
+    400: "#AFAFAF",
+    /** Value: #757575*/
+    500: "#757575",
+    /** Value: #424C55*/
+    600: "#424C55",
+    /** Value: #2E2F31*/
+    700: "#2E2F31",
+    /** Value: #1A1A1A*/
+    800: "#1A1A1A",
+  },
+};
--- theme.ts
+++ theme.ts
@@ -2,17 +2,21 @@
 // https://next.material-ui.com/customization/default-theme/
 // https://chakra-ui.com/docs/theming/theme
 // https://material.io/design/material-theming/overview.html#using-material-theming
+// https://theme-ui.com/
+// https://jxnblk.com/blog/design-graph/
+// https://tailwindcss.com/
+
 // Suggested Guidelines:
 // - When creating sizes that have numerical values that are not on a incremental scale (2, 4, 6, 8, 10, 12 = increments of 2) use t-shirt starting at xs and ending at xl while anything passed that should be a multiplication of the smallest size (xs). For example is the xs sizes is 2 pixels and xl is 16 pixels, once you need more sizes and say theres a sizes that is 48 pixels being represented in the design it would be the 48 pixels divided by the 2 pixels and would give you the labels as x24 or 24 times the smallest size.
 // - When dealing with values represented as a scale in Figma, like increments of 100, use a numbered scale to replicate the ideas in the design.
 // - Try to match the verbiage as it is found in Figma to help maintain a consistent dialogue between all parties
 // - TBD i hope this is a easy way to get feedback
 
-import { DefaultTheme } from "styled-components";
-import { colors, sizes } from "../index";
+// import { DefaultTheme } from "styled-components";
+// import { colors, sizes } from "../index";
 
 // Focuses on generics and groups of basic or generalized implementation without being too specific about concepts that aren't created yet
-const globalTheme: DefaultTheme = {
+const globalTheme = {
   // Used for standardizing the appearance of how things should appear like they are "elevating" of of the page but this would also include elevation changes for interactive states as well...box shadows
   elevation: {
     0: "placeholder value", // Use for: Flat Object (to reset or override elevation/box shadow need be)
@@ -22,16 +26,30 @@ const globalTheme: DefaultTheme = {
     4: "placeholder value", // Use for: Modals
   },
   // Groups of colors specific to general and reused implementation details. The colors here should be found and have the intent of being used for MORE than just one property like, border-color, background-color, fill, color, shadows, etc or just a specific component. Colors here should NOT be focuses on specific properties alone (borders, backgrounds, text colors, etc) or components like (header, calendar, inputs, etc) but should be inserted based on relation to the platform as a whole or similar responses are actions done by the user where variations exits like (error, success are variations in response and rewards and loyalty and brand are specific groups of colors found in various or very unique places within the platform). For example the brand.primary color can be used for a border color, background color and foreground color on various elements and components.
+  // Suggested "rules":
+  // - Ways to establish colors:
+  // -- Order (primary, secondary, tertiary). If more than 3 options are available avoid this concept and see if any of the others below could work.
+  // -- Shades where the "base" color variant in hue or saturation (default, light, dark or number values like 100, 200, 300, 400, 500)
+  // -- Related use that align with the brand, platform or implementation details (members, loyalty, vip, brand)
+  // -- Opposites/complementary (default, inverted). If more then 2 values are available the prefix the inverted value with the name like memberInverted. If the value is a color name, do not prefix inverted with that value because blackInverted doesn't make sense...it is white.
   palettes: {
+    // TBD
+    accent: {
+      light: "placeholder value (green-300)",
+      default: "placeholder value (green-400)",
+      dark: "placeholder value (green-500)",
+    },
+    // Order (primary, secondary, tertiary). If more than 3 options are available avoid this concept and see if any of the others below could work.
     brand: {
       primary: "placeholder value",
       secondary: "placeholder value",
       tertiary: "placeholder value",
     },
     loyalty: {
-      primary: "placeholder value",
-      background: "placeholder value",
+      light: "placeholder value",
+      dark: "placeholder value",
     },
+    // Related use that align with the brand, platform or implementation details (members, loyalty, vip, brand)
     rewards: {
       member: "placeholder value",
       silver: "placeholder value",
@@ -44,6 +62,7 @@ const globalTheme: DefaultTheme = {
         default: "placeholder value",
         inverted: "placeholder value",
       },
+      // Opposites/complementary (default, inverted). If more then 2 values are available the prefix the inverted value with the name like memberInverted. If the value is a color name, do not prefix inverted with that value because blackInverted doesn't make sense...it is white.
       secondary: {
         default: "placeholder value",
         inverted: "placeholder value",
@@ -68,21 +87,6 @@ const globalTheme: DefaultTheme = {
         dark: "placeholder value", // Background and border for default and light version
       },
     },
-    // This is on the edge of "breaking the rule" of being too specific to implementation
-    text: {
-      primary: {
-        default: "placeholder value",
-        inverted: "placeholder value",
-      },
-      secondary: {
-        default: "placeholder value",
-        inverted: "placeholder value",
-      },
-      tertiary: {
-        default: "placeholder value",
-        inverted: "placeholder value",
-      },
-    },
   },
   // Defines basic shapes of the bounding box of DOM nodes. Right now its just all around border radius but it could evolve to have only 2 or 3 corners for tooltips, clipping masks, tabs, artifacts, etc.
   shape: {
@@ -93,58 +97,63 @@ const globalTheme: DefaultTheme = {
       x50: "placeholder value", // Use for: Group Buttons, Tags
       x100: "placeholder value", // Use for circles
     },
+    borderWidth: {},
   },
+  // Generic spacing used on various elements throughout the platform
+  space: (a) => a * 8 + "px",
   // Typography is the root here cause it explicity state that you're trying to apply styles to the appearance of text and text alone. It's broken out by attribute instead of by element because all of the HTML elements DON'T have different font styling attributes. This also abstracts away the semantic/markup decisions being made by the theme vs upon component implementation i.e. all weights are the same for headings and body copy. Organized for ways that aid with intellisense/autofill so if you're trying to apply a property to text element you type typography, then you get suggested a font styled property like color, font-size, font-weight, line-height, letter-spacing, etc. From there you look for the type or group of text that you're style should fit within if applicable, like display, heading, body, etc. From there you should then see what options are available from the established UX Design System found in Figma .
-  typography: {
-    // Sizes as stand alone cause not all HTML elements have different font styling attributes. i.e. all weights are the same for headings and body copy
-    h1: {
-      fontSize: "placeholder value",
-      lineHeight: "placeholder value",
-    },
-    h2: {
-      fontSize: "placeholder value",
-      lineHeight: "placeholder value",
-    },
-    h3: {
-      fontSize: "placeholder value",
-      lineHeight: "placeholder value",
-    },
-    h4: {
-      fontSize: "placeholder value",
-      lineHeight: "placeholder value",
-    },
-    h5: {
-      fontSize: "placeholder value",
-      lineHeight: "placeholder value",
-    },
-    // This is from Figma and extends past a h1 so idk what to do here
-    display: {
-      md: {
-        fontSize: "placeholder value",
-        lineHeight: "placeholder value",
-        letterSpacing: "placeholder value",
-      },
-      lg: {
-        fontSize: "placeholder value",
-        lineHeight: "placeholder value",
-        letterSpacing: "placeholder value",
-      },
-    },
-    // This is from Figma but idk if we'll get the same arguments here for the headings as in theory this should be a p tag but its usage and semantics sound be out of sync cause body copy could be used in various places like a span tag, small tag, label tag, caption tag, dt, dd tag, table elements, etc.
-    body: {
-      sm: {
-        fontSize: "placeholder value",
-        lineHeight: "placeholder value",
-      },
-      md: {
-        fontSize: "placeholder value",
-        lineHeight: "placeholder value",
-      },
-      lg: {
-        fontSize: "placeholder value",
-        lineHeight: "placeholder value",
-      },
-    },
+  // TODO: Convert these to a scale for font-size, line-height, font-weight
+  fontSize: {
+    /** Value: 12px */
+    xs: "12px",
+    /** Value: 14px */
+    sm: "14px",
+    /** Value: 16px */
+    md: "16px",
+    /** Value: 20px */
+    lg: "20px",
+    /** Value: 24px */
+    xl: "24px",
+    /** Value: 36px */
+    x3: "36px", // Smallest value (12) times 3
+    /** Value: 72px */
+    x6: "72px", // Smallest value (12) times 6
+  },
+  lineHeight: {
+    /** Value: 16px */
+    xs: "16px",
+    /** Value: 20px */
+    sm: "20px",
+    /** Value: 24px */
+    md: "24px",
+    /** Value: 28px */
+    lg: "28px",
+    /** Value: 46px */
+    xl: "46px",
+    /** Value: 80px */
+    x5: "80px", // Smallest value (16) times 5
+  },
+  fontWeight: {
+    /** Value: 100 */
+    thin: "100", // Thin (Hairline)
+    /** Value: 200 */
+    extraLight: "200", // Extra Light (Ultra Light)
+    /** Value: 300 */
+    light: "300", // Light
+    /** Value: 400 */
+    normal: "400", // Normal (Regular)
+    /** Value: 500 */
+    medium: "500", // Medium
+    /** Value: 600 */
+    semiBold: "600", // Semi Bold (Demi Bold)
+    /** Value: 700 */
+    bold: "700", // Bold
+    /** Value: 800 */
+    extraBold: "800", // Extra Bold (Ultra Bold)
+    /** Value: 900 */
+    black: "900", // Black (Heavy)
+    /** Value: 950 */
+    extraBlack: "950", // Extra Black (Ultra Black)
   },
 };
 
