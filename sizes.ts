8aac951ad6779f3e8c8ed108e28ee8b008ce38cf
mattstraussHotelEngine <80705586+mattstraussHotelEngine@users.noreply.github.com>
2021-08-26 10:47:42 -0600

    Updated sizes to have as const also

--- sizes.ts
+++ sizes.ts
@@ -1,36 +1,22 @@
 // NOTE: This file needs to be considered to be either deleted or modified as the sizes in here were mostly moved to the theme
 export const breakpoints = {
-  /** Value: 320 */
   xs: 320,
-  /** Value: 480 */
   sm: 480,
-  /** Value: 576 */
   md: 576,
-  /** Value: 768 */
   lg: 768,
-  /** Value: 992 */
   xl: 992,
-  /** Value: 1200 */
   xxl: 1200,
-  /** Value: 1400 */
   xxxl: 1400,
-};
+} as const;
 
 export const sizes = {
   breakpoints: {
-    /** Value: 320px */
     xs: `${breakpoints.xs}px`,
-    /** Value: 480px */
     sm: `${breakpoints.sm}px`,
-    /** Value: 576px */
     md: `${breakpoints.md}px`,
-    /** Value: 768px */
     lg: `${breakpoints.lg}px`,
-    /** Value: 992px */
     xl: `${breakpoints.xl}px`,
-    /** Value: 1200px */
     xxl: `${breakpoints.xxl}px`,
-    /** Value: 1400px */
     xxxl: `${breakpoints.xxxl}px`,
   },
-};
+} as const;
